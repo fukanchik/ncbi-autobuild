608299eef638aeca42beafaca8b3a01d57092e59
Sergey Fukanchik <fukanchi@ncbi.nlm.nih.gov>
2017-01-11 18:15:26 -0500

    Add check_r

--- build_deb.sh
+++ build_deb.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 
+[ -z "$1" ] && exit 1
+
+export PLATFORM=$1
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install svn
+sudo apt-get -y install subversion
 sudo apt-get -y install build-essential
 
 . test.sh
