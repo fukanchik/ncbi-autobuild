6b471f00037cc9534872f3d37ffaee9d7e0b2084
Sergey Fukanchik <fukanchi@ncbi.nlm.nih.gov>
2017-01-11 17:06:12 -0500

    Fix cd before build

---
+++ travis.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+
+script=$(readlink -f $0)
+script_dir=$(dirname $script)
+
+TRAVIS_REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2)
+DOCKER_REPO=tarantool/build
+
+DOCKER_EXTRA=""
+
+case ${PACK} in
+    deb)
+        TARANTOOL_IMAGE=${DOCKER_REPO}:${OS}-${DIST}
+        DOCKER_EXTRA="RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections"
+        ;;
+    rpm)
+        TARANTOOL_IMAGE=${DOCKER_REPO}:${OS}${DIST}
+        ;;
+    *)
+        echo "Packaging ${PACK} is not supported."
+        echo 1
+        ;;
+esac
+
+mkdir shared
+
+cp ${scipt_dir}/build_${PACK}.sh shared
+
+echo "FROM ${TARANTOOL_IMAGE}" > Dockerfile
+echo "RUN useradd -s ${SHELL} -u $(id -u) -d ${HOME} ${USER}" >> Dockerfile
+[ -z "${DOCKER_EXTRA}" ] || echo "${DOCKER_EXTRA}" >> Dockerfile
+echo "RUN usermod -a -G wheel ${USER} || :;\\" >> Dockerfile
+echo "    usermod -a -G adm ${USER} || :;\\" >> Dockerfile
+echo "    usermod -a -G sudo ${USER} || :;\\" >> Dockerfile
+echo "    usermod -a -G mock ${USER} || :" >> Dockerfile
+echo "USER ${USER}" >> Dockerfile
+
+
+echo "Dockerfile:"
+
+cat Dockerfile
+
+mv Dockerfile shared/
+
+docker build --rm=true --quiet=true -t ${TARANTOOL_IMAGE}-for-${USER} shared
+
+docker run --volume ${PWD}/shared:/home/${USER}/result --workdir /home/${USER}/result --rm=true --user=${USER} ${TARANTOOL_IMAGE}-for-${USER} bash -x /home/${USER}/result/build_${PACK}.sh "${PLATFORM}"
+
