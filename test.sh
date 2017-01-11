a7237be45f5270edaa50f5c50c6a888ad3e6fde6
Sergey Fukanchik <fukanchi@ncbi.nlm.nih.gov>
2017-01-11 15:16:45 -0500

    Attempt to build

--- test.sh
+++ test.sh
@@ -1,5 +1,19 @@
 #! /usr/bin/env bash
+
+ls
+
 svn co http://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++ toolkit-svn
+
+ls
+
 cd  toolkit-svn
+
+ls
+
 ./compilers/unix/GCC.sh --with-flat-makefile
 
+ls
+
+(cd toolkit-svn/*-DebugMT64/build && /usr/bin/make -f Makefile.flat)
+
+ls
