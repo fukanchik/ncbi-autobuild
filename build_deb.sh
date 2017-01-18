57ae386ec7e185751bedceeca5ac2431c284c363
Sergey Fukanchik <fukanchi@ncbi.nlm.nih.gov>
2017-01-18 15:31:08 -0500

    Add apt-get install less + sed

--- build_deb.sh
+++ build_deb.sh
@@ -8,6 +8,8 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install subversion
 sudo apt-get -y install build-essential
+sudo apt-get -y install less
+sudo apt-get -y install sed
 
 . test.sh
 
