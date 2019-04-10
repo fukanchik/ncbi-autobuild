ec79793dcd32231c65066ce79bc35abfe92b7a8e
Sergey Fukanchik <fukanchi@ncbi.nlm.nih.gov>
2019-04-10 11:42:00 -0400

    Refreshing to not use tarantool

--- travis.sh
+++ travis.sh
@@ -3,32 +3,12 @@
 script=$(readlink -f $0)
 script_dir=$(dirname $script)
 
-TRAVIS_REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2)
-DOCKER_REPO=tarantool/build
-
-DOCKER_EXTRA=""
-
-case ${PACK} in
-    deb)
-        TARANTOOL_IMAGE=${DOCKER_REPO}:${OS}-${DIST}
-        DOCKER_EXTRA="RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections"
-        ;;
-    rpm)
-        TARANTOOL_IMAGE=${DOCKER_REPO}:${OS}${DIST}
-        ;;
-    *)
-        echo "Packaging ${PACK} is not supported."
-        echo 1
-        ;;
-esac
-
 mkdir shared
 
-cp "${script_dir}/build_${PACK}.sh" "${script_dir}/test.sh" shared
+DOCKER_IMAGE='${OS}:${DIST}'
 
-echo "FROM ${TARANTOOL_IMAGE}" > Dockerfile
+echo "FROM ${DOCKER_IMAGE}" > Dockerfile
 echo "RUN useradd -s ${SHELL} -u $(id -u) -d ${HOME} ${USER}" >> Dockerfile
-[ -z "${DOCKER_EXTRA}" ] || echo "${DOCKER_EXTRA}" >> Dockerfile
 echo "RUN usermod -a -G wheel ${USER} || :;\\" >> Dockerfile
 echo "    usermod -a -G adm ${USER} || :;\\" >> Dockerfile
 echo "    usermod -a -G sudo ${USER} || :;\\" >> Dockerfile
@@ -43,7 +23,7 @@ cat Dockerfile
 mv Dockerfile shared/
 cp test.sh build_deb.sh build_rpm.sh shared/
 
-docker build --rm=true --quiet=true -t ${TARANTOOL_IMAGE}-for-${USER} shared
+docker build --rm=true --quiet=true -t ${DOCCKER_IMAGE}-for-${USER} shared
 
-docker run --name cxxbuild --rm=true --volume ${PWD}/shared:/home/${USER}/result --workdir /home/${USER}/result --user=${USER} ${TARANTOOL_IMAGE}-for-${USER} bash -x /home/${USER}/result/build_${PACK}.sh "${PLATFORM}"
+docker run --name cxxbuild --rm=true --volume ${PWD}/shared:/home/${USER}/result --workdir /home/${USER}/result --user=${USER} ${DOCKER_IMAGE}-for-${USER} bash -x /home/${USER}/result/build_${PACK}.sh "${PLATFORM}"
 
